c347820a066b54a4798cd7bc40b3263f5781815d
Manasbekoff <manasbekov@gmail.com>
2020-08-29 18:37:23 +0600

    Final

--- app/src/main/java/com/example/android1hw31/MainActivity.java
+++ app/src/main/java/com/example/android1hw31/MainActivity.java
@@ -1,23 +1,21 @@
 package com.example.android1hw31;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import static com.example.android1hw31.MainActivity2.EXTRA_KEY;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     public final static int SECOND_ACTIVITY_REQUEST_CODE = 100;
 
-    private Button btnTwo;
     private TextView textView;
 
 
@@ -32,13 +30,28 @@ public class MainActivity extends AppCompatActivity {
 
     private void summary() {
         Button btnOne = findViewById(R.id.btnOne);
+        Button btnTwo = findViewById(R.id.btnTwo);
         textView = findViewById(R.id.txtView);
+        btnTwo.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("IntentReset")
+            @Override
+            public void onClick(View view) {
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setData((Uri.parse("Mail to:")));
+                emailIntent.setType("text/plain");
+                try {
+                    startActivity(Intent.createChooser(emailIntent, "Choose an email client"));
+                    finish();
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(MainActivity.this, "App is not installed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         btnOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 startActivityForResult(intent, SECOND_ACTIVITY_REQUEST_CODE);
-                finish();
             }
         });
 
@@ -48,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
             if (requestCode == SECOND_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
                 String returnResult = data.getStringExtra(MainActivity2.EXTRA_KEY);
                 textView.setText(returnResult);
--- app/src/main/java/com/example/android1hw31/MainActivity2.java
+++ app/src/main/java/com/example/android1hw31/MainActivity2.java
@@ -1,11 +1,11 @@
 package com.example.android1hw31;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,9 +13,11 @@ public class MainActivity2 extends AppCompatActivity {
 
     public final static String EXTRA_KEY = "KEY";
 
-    private TextView textView;
     private EditText editText1;
     private EditText editText2;
+    int one;
+    int two;
+    String sum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +43,13 @@ public class MainActivity2 extends AppCompatActivity {
                 }
 
 
+            @SuppressLint("SetTextI18n")
             public void sendResult(){
-                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
-                 String message = editText1.getText().toString() ;
-                intent.putExtra(EXTRA_KEY, message);
+                Intent intent = new Intent();
+                one = Integer.parseInt(editText1.getText().toString());
+                two = Integer.parseInt(editText2.getText().toString());
+                sum=(String.valueOf(one + two));
+                intent.putExtra(EXTRA_KEY, sum);
                 setResult(RESULT_OK, intent);
                 finish();
             }
